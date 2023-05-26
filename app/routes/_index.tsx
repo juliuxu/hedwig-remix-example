6c70b78306cbc21284ed2352c640559c1cc4c166
Julian Jark <julian.jark@posten.no>
2023-05-26 22:34:13 +0200

    use other components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,24 @@ import "@posten-hedwig/core";
 import "@posten-hedwig/base";
 import { Accordion, AccordionItem } from "@posten-hedwig/accordion";
 import type { V2_MetaFunction } from "@remix-run/node";
+import {
+  Badge,
+  DarkBadge,
+  WarningBadge,
+  WhiteBadge,
+} from "@posten-hedwig/badge";
+import { Block } from "@posten-hedwig/block";
+import {
+  ChatButton,
+  LightOutlineButton,
+  PrimaryButton,
+  PrimaryOutlineButton,
+  SecondaryButton,
+  SecondaryOutlineButton,
+} from "@posten-hedwig/button";
+import { Container } from "@posten-hedwig/container";
+import { Grid, GridItem } from "@posten-hedwig/grid";
+import InfoBox from "@posten-hedwig/infobox";
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -15,7 +33,7 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Index() {
   return (
-    <main>
+    <Container as="main">
       <article>
         <h2>Accordion</h2>
         <Accordion>
@@ -27,6 +45,47 @@ export default function Index() {
           </AccordionItem>
         </Accordion>
       </article>
-    </main>
+
+      <article>
+        <h2>Badge</h2>
+        <Badge>Badge</Badge>
+        <DarkBadge>DarkBadge</DarkBadge>
+        <WarningBadge>WarningBadge</WarningBadge>
+        <WhiteBadge>WhiteBadge</WhiteBadge>
+      </article>
+
+      <article>
+        <h2>Block</h2>
+        <Block elementType="p">Hello, world</Block>
+      </article>
+
+      <article>
+        <h2>Button</h2>
+        <ChatButton />
+        <LightOutlineButton>LightOutlineButton</LightOutlineButton>
+        <PrimaryButton>PrimaryButton</PrimaryButton>
+        <PrimaryOutlineButton>PrimaryOutlineButton</PrimaryOutlineButton>
+        <SecondaryButton>SecondaryButton</SecondaryButton>
+        <SecondaryOutlineButton>SecondaryOutlineButton</SecondaryOutlineButton>
+      </article>
+
+      <article>
+        <h2>Grid</h2>
+        <Grid>
+          <GridItem size="one-half">Left</GridItem>
+          <GridItem size="one-half">Right</GridItem>
+        </Grid>
+        <Grid>
+          <GridItem size="one-third">First</GridItem>
+          <GridItem size="one-third">Second</GridItem>
+          <GridItem size="one-third">Third</GridItem>
+        </Grid>
+      </article>
+
+      <article>
+        <h2>InfoBox</h2>
+        <InfoBox title="Ahoi!">Here is some info</InfoBox>
+      </article>
+    </Container>
   );
 }
